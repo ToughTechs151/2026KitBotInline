d83d183c727787130b3ccf0aac5c83f7b6adcc02
jwt388 <jtnh388@gmail.com>
2026-01-19 21:48:00 -0500

    Use bus voltage for output

--- src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
@@ -107,8 +107,10 @@ public class CANFuelSubsystem extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
     SmartDashboard.putNumber("LauncherCurrent", intakeLauncherRoller.getOutputCurrent());
-    SmartDashboard.putNumber("LauncherVoltage", intakeLauncherRoller.getAppliedOutput() * 12); // 12 = nominal voltage
+    SmartDashboard.putNumber("LauncherVoltage", intakeLauncherRoller.getAppliedOutput() 
+    * intakeLauncherRoller.getBusVoltage());
     SmartDashboard.putNumber("FeederCurrent", feederRoller.getOutputCurrent());
-    SmartDashboard.putNumber("FeederVoltage", feederRoller.getAppliedOutput() * 12); // 12 = nominal voltage
+    SmartDashboard.putNumber("FeederVoltage", feederRoller.getAppliedOutput() 
+    * feederRoller.getBusVoltage());
   }
 }
